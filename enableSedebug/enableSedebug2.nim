830d91b3ea154b15cb1374f1a20c5da21efb5dd1
microvorld <870059622@qq.com>
2021-08-31 15:12:12 +0800

    Create enableSedebug2.nim

---
+++ enableSedebug/enableSedebug2.nim
@@ -0,0 +1,73 @@
+when not defined(c):
+    {.error: "Must be compiled in c mode"}
+
+{.emit: """
+
+#include <windows.h>
+#include <stdio.h>
+#pragma comment(lib, "cmcfg32.lib")
+
+BOOL SetPrivilege(
+    HANDLE hToken,          // access token handle
+    LPCTSTR lpszPrivilege,  // name of privilege to enable/disable
+    BOOL bEnablePrivilege   // to enable or disable privilege
+    ) 
+{
+    TOKEN_PRIVILEGES tp;
+    LUID luid;
+
+    if ( !LookupPrivilegeValue( 
+            NULL,            // lookup privilege on local system
+            lpszPrivilege,   // privilege to lookup 
+            &luid ) )        // receives LUID of privilege
+    {
+        printf("LookupPrivilegeValue error: %u\n", GetLastError() ); 
+        return FALSE; 
+    }
+
+    tp.PrivilegeCount = 1;
+    tp.Privileges[0].Luid = luid;
+    if (bEnablePrivilege)
+        tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+    else
+        tp.Privileges[0].Attributes = 0;
+
+    // Enable the privilege or disable all privileges.
+
+    if ( !AdjustTokenPrivileges(
+           hToken, 
+           FALSE, 
+           &tp, 
+           sizeof(TOKEN_PRIVILEGES), 
+           (PTOKEN_PRIVILEGES) NULL, 
+           (PDWORD) NULL) )
+    { 
+          printf("AdjustTokenPrivileges error: %u\n", GetLastError() ); 
+          return FALSE; 
+    } 
+
+    if (GetLastError() == ERROR_NOT_ALL_ASSIGNED)
+
+    {
+          printf("The token does not have the specified privilege. \n");
+          return FALSE;
+    } 
+
+    system("whoami /priv | findstr SeDebugPrivilege");
+
+    return TRUE;
+}
+
+
+void enableSedebug( )
+{
+	HANDLE hToken;
+	BOOL bRet=OpenProcessToken(GetCurrentProcess(),TOKEN_ALL_ACCESS,&hToken);
+	SetPrivilege(hToken,SE_DEBUG_NAME,TRUE);
+}
+
+""".}
+proc enableSedebug(): void
+    {.importc: "enableSedebug", nodecl.}
+when isMainModule:
+  enableSedebug()
